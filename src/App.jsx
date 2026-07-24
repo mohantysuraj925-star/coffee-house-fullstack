3bd2a3f0a481033c38b89149bcf80d344c8c6b34
Suraj Kumar Mohanty <suraj@example.com>
2026-07-24 21:55:25 +0530

    Added Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import AllRoutes from "./routes/AllRoutes";
 import Navbar from "./components/common/Navbar";
 import { useLocation } from "react-router-dom";
 import Footer from "./components/common/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const location = useLocation();
-
   const isAdminRoute = location.pathname.startsWith("/admin");
 
   return (
     <>
       {!isAdminRoute && <Navbar />}
-
       <AllRoutes />
       {!isAdminRoute && <Footer />}
+      <Analytics />
     </>
   );
 }
